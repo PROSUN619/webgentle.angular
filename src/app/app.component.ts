65fbb6469905a7e88db3de28b65ed6b7bd549719
PROSUN619 <prosun.chakraborty92@gmail.com>
2022-08-27 18:45:15 +0530

    video 14 : event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,9 @@ export class AppComponent {
     name: 'Prasun',
     designation: 'Analyst',
     salary: 1000,
-    location: 'Kolkata'
+    location: 'Kolkata',
+    src: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+    alt: 'profile pic'
   }
 
   getData(data: number): string {
@@ -22,5 +24,20 @@ export class AppComponent {
       return "The number is " + data;
     else
       return "Other Number " + data;
+  }
+
+  // event 
+
+  event : string = '';  
+  onclick() : void{
+    this.event = 'Clicked';
+  }
+
+  onMouseHover() : void{
+    this.event = 'Mouse Hovered';
+  }
+
+  onTextChange() : void{
+    this.event = 'Text changed';
   }
 }
