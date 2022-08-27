e6c844f4c89991902ace6f3a1ecd6e8f7432603d
PROSUN619 <prosun.chakraborty92@gmail.com>
2022-08-27 19:00:25 +0530

    video 15 : property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,38 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name: string = "Prasun"
 
-  employees: string[] = ['Ram', 'Laxman', 'Sita', 'Hanuman']
+  btnId: string = 'btnSave';
+  textValue: string = '';
+  isDisabled: boolean = true;
 
-  employee = {
-    name: 'Prasun',
-    designation: 'Analyst',
-    salary: 1000,
-    location: 'Kolkata',
-    src: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
-    alt: 'profile pic'
-  }
-
-  getData(data: number): string {
-    if (data === 1)
-      return "The number is " + data;
+  handleOnChange(): void {
+    
+    if (this.textValue.length > 0)
+      this.isDisabled = true;
     else
-      return "Other Number " + data;
+      this.isDisabled = false;
   }
 
-  // event 
-
-  event : string = '';  
-  onclick() : void{
-    this.event = 'Clicked';
-  }
-
-  onMouseHover() : void{
-    this.event = 'Mouse Hovered';
-  }
-
-  onTextChange() : void{
-    this.event = 'Text changed';
-  }
 }
