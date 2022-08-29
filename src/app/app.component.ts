fe008194bc3bbd602470f463075aa72bdb72deec
PROSUN619 <prosun.chakraborty92@gmail.com>
2022-08-29 21:49:32 +0530

    video: 17, Class binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,13 +10,19 @@ export class AppComponent {
   btnId: string = 'btnSave';
   textValue: string = '';
   isDisabled: boolean = true;
+  myClass: string = 'textColor textHeight'
+  myClassArr: string[] = ['textColor', 'textHeight']
+  myClassObj: any = { textColor: true, textHeight: this.foo() };
 
   handleOnChange(): void {
-    
+
     if (this.textValue.length > 0)
       this.isDisabled = true;
     else
       this.isDisabled = false;
   }
 
+  foo():boolean{
+    return false;
+  }
 }
